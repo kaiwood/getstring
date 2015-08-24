36b86807220366442900494f505c2af522cc747e
Kai Wood <kwood@kwd.io>
2015-08-24 15:11:22 +0200

    Add additional parameter to make validation of input possible
    See docs for usage.

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 /**
- * Prompt for input
- * @param question
- * @param callback
+ * Prompt for input.
+ *
+ * If the optional validato
+ * @param {string} question
+ * @param {function} [validator]
+ * @callback callback
  */
-var getstring = function(question, callback) {
+var getstring = function() {
+    var question = arguments[0];
+    var callback = arguments[arguments.length-1]
+
+    var validator = arguments.length > 2 ? arguments[1] : false;
+
+
     process.stdout.write(question + " ");
     process.stdin.once('data', function (data) {
-        callback(data.toString().trim());
+
+        if (validator && ! validator(data)) {
+            getstring(question, validator, callback);
+        } else {
+            callback(data.toString().trim());
+        }
     });
 };
 
--- test/getstring_test.js
+++ test/getstring_test.js
@@ -18,4 +18,24 @@ describe("Prompting the user for input", function () {
             done();
         });
     });
+
+    it("should be possible to give an additional argument to validate the input", function (done) {
+        function validateInput(input) {
+            var number = parseInt(input);
+            if (isNaN(number)) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        process.nextTick(function () {
+            stdin.send("123");
+        });
+
+        gets("Give me a number", validateInput, function (answer) {
+            expect(answer).to.equal("123");
+            done();
+        });
+    })
 });
